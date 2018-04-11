4768316c241fd655503dfc198dd846751a7e8004
Irin <mpg.students+borden@gmail.com>
2018-04-11 13:05:31 -0400

    site url

--- lib/project_admin_toggle.php
+++ lib/project_admin_toggle.php
@@ -5,10 +5,11 @@ function get_admin_toggle($userGuid){
   $return = array();
 
   $isAdmin = (get_entity($userGuid)->project_admin == '1');
-  
+
   $vars = array('userGuid'=>$userGuid);
   if($isAdmin){
     $title = "You are a Project Admin";
+    $title = elgg_get_site_url(0);
     $content .= elgg_view_form('project_admin_toggle/removeProjectAdmin', array(), $vars);
   }else{
     $title = "You are NOT a Project Admin";
